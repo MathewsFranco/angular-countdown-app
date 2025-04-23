33acbc2adc20f9080729b93a8303584cd423f379
Andressa <andressatreis@gmail.com>
2025-04-23 16:48:22 +0200

    ✨ add font and colour variables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, NgModule } from '@angular/core'
+import { Component, NgModule, ViewEncapsulation } from '@angular/core'
 
 import { CountdownComponent } from './components/countdown/countdown.component'
 import { EventTitleComponent } from './components/event-title/event-title.component'
@@ -8,7 +8,9 @@ import { FormComponent } from './components/form/form.component'
   selector: 'app-root',
   standalone: true,
   imports: [CountdownComponent, EventTitleComponent, FormComponent],
-  template: '<app-event-title/><app-countdown/><app-countdown-form/>',
+  template:
+    '<div class="container"><app-event-title/><app-countdown/><app-countdown-form/></div>',
   styleUrl: './app.component.scss',
+  encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent {}
